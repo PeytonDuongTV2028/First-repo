7999823bca758c04661436ba9acc58cab7fe39ba
PeytonDuongTV2028 <depatience32@gmail.com>
2025-02-13 10:47:22 -0700

    I made joke

---
+++ boxPrint.py
@@ -0,0 +1,10 @@
+# def boxPrint():
+#     txt = "welcome to the jungle"
+#     x = txt.split()
+#     print (txt.split())
+
+#     print ('***********')
+#     print('\n'.join(txt.split())) # This works really well to print each word down vertically one by one
+#     print ('***********')
+
+# boxPrint()
